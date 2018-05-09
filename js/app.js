738e8422f3946b7f35fecebfa6c1b0f411fec0ac
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-08 20:26:50 -0500

    nav links will always redirect to top of section whether or not navbar is deployed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// console.log("js connected");
+// check if window is below threshold of navbar-expand-md (767px)
+function checkWidth () {
+	// toggle between nav heights depending on if navbar is deployed or not
+	if ($(window).width() <= 767) {
+		$(".anchor").css("top", "-176px");
+	} else {
+		$(".anchor").css("top", "-57px");
+	}
+}
+checkWidth();
+
+// check width again if window is resized
+$(window).on("resize", () => {
+	checkWidth();
+});
