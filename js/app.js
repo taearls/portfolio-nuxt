6610d7b25fdf0d5b45001e5cff6277caa3e7bbc2
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-09 10:02:38 -0500

    website body responds to navbar dropdown being toggled

--- js/app.js
+++ js/app.js
@@ -13,3 +13,24 @@ checkWidth();
 $(window).on("resize", () => {
 	checkWidth();
 });
+
+// listen for toggler button in nav being clicked using boolean variable
+let navDeployed = false;
+
+$("#toggler").on("click", () => {
+	navDeployed = !navDeployed;
+	// check for home button being clicked
+	$("#toggler").css("transition", "0.5s ease");
+	if (navDeployed) {
+		$("body").css({
+			"padding-top": "176px",
+			"transition": "0.35s ease"
+		})
+	} else {
+		$("body").css({
+			"padding-top": "57px",
+			"transition": "0.35s ease"
+		})
+	}
+});
+
