c89afa533e6fcdc58f8567a2dc08e27d11416fae
Tyler Earls <tyler.a.earls@gmail.com>
2018-08-18 01:52:47 -0500

    only track scss files being converted to css

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,16 @@ gulp.task("scss", () => {
 	// find the file we want to convert
 	// then we need to convert
 	// then we need to send it to where we want
+
+  // convert style.scss
 	gulp.src("./scss/style.scss")
 		.pipe(scss())
 		.pipe(gulp.dest("./css/"));
+
+  // convert fontawesome.scss
+  gulp.src("./scss/fontawesome.scss")
+    .pipe(scss())
+    .pipe(gulp.dest("./css/"));
 })
 
 gulp.task("watch", () => {
