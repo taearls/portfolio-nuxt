5e0e75110ca49a3ea0324b6ad28c2ead7c1693d5
Tyler Earls <tyler.a.earls@gmail.com>
2020-07-22 22:50:58 -0500

    update footer

--- components/global/VueFooter.vue
+++ components/global/VueFooter.vue
@@ -2,7 +2,7 @@
   <footer>
     <social-media-icons />
     <h6 
-      class="text-center"
+      class="text-center text-xs tracking-wide font-light"
       v-html="footerText"
     />
   </footer>
@@ -18,7 +18,7 @@ export default {
   computed: {
     footerText() {
       const year = new Date().getFullYear();
-      return `&copy; Tyler Earls ${year}`;
+      return `&copy; 1993-${year} &bull; Tyler Earls`;
     },
   },
 
