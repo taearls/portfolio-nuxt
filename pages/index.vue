16aa63b375610cc9c5693fcb3fe243f22afdbdeb
Tyler Earls <tyler.a.earls@gmail.com>
2020-08-09 21:29:01 -0500

    adjust line-height in paragraphs, milestone for home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="px-4 mx-auto max-w-none w-4/5 leading-8">
+    <!-- TODO: add images to break up text -->
     <h1 class="text-left text-purple-700 font-extrabold text-4xl leading-tight">
       Hi there! 👋
       <br>
@@ -7,7 +8,7 @@
       <br>
       I am a software engineer and musician.
     </h1>
-    <p class="text-soft-black dark:text-white my-5 text-lg">
+    <p class="text-soft-black dark:text-white my-4 text-lg leading-normal">
       I work at <a
         class="rounded-sm focus:outline-none focus:shadow-outline"
         href="https://www.ensighten.com"
@@ -15,10 +16,10 @@
         target="_blank"
       >Ensighten</a>, where I help provide client-side security and privacy law compliance for Fortune 500 companies. I enjoy the continuously evolving challenge of making the web safe for online users and their personal data.
     </p>
-    <p class="text-soft-black dark:text-white my-5 text-lg">
+    <p class="text-soft-black dark:text-white my-4 text-lg leading-normal">
       Occasionally, I do freelance frontend work on the side. I love to build UI components that people interact with on the web.
     </p>
-    <p class="text-soft-black dark:text-white my-5 text-lg">
+    <p class="text-soft-black dark:text-white my-4 text-lg leading-normal">
       Outside of tech, I write songs as Cuckoo and the Birds. You can listen to my music <a
         class="rounded-sm focus:outline-none focus:shadow-outline"
         href="https://cuckooandthebirds.bandcamp.com"
