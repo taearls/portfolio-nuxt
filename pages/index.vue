6c23a6a1aba5cb75f80db3bfac0f8bbaab25cfa4
Tyler Earls <tyler.a.earls@gmail.com>
2020-08-16 16:46:31 -0500

    update copy on about page

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,53 @@
 <template>
   <section class="px-4 mt-20 mx-auto max-w-none w-4/5 leading-8">
     <!-- TODO: add images to break up text -->
-    <h1 
+    <h1
       class="text-left mb-4 text-purple-700 dark:text-purple-500 font-extrabold text-4xl"
       style="line-height: 1.1;"
     >
       Hi there! 👋
-      <br>
-      I'm Tyler Earls.
-      <br>
+      <br />
+      My name is Tyler Earls.
+      <br />
       I am a software engineer and musician.
     </h1>
     <p class="text-soft-black dark:text-white mt-4 text-lg md:text-xl leading-normal">
-      I work at <a
+      Currently, I work at
+      <a
         class="rounded-sm text-purple-700 dark:text-purple-500 font-semibold hover:text-blue-500 dark-hover:text-blue-300 focus:outline-none focus:shadow-outline-light dark-focus:shadow-outline-dark"
         href="https://www.ensighten.com"
         rel="noreferrer"
         target="_blank"
-      >Ensighten</a>, where I help provide client-side security and privacy law compliance for Fortune 500 companies. I enjoy the continuously evolving challenge of making the web safe for online users and their personal data.
+        >Ensighten</a
+      >, where I help provide client-side security and privacy law compliance for our clients. I
+      enjoy the continuously evolving challenge of making the web safe for online users and their
+      personal data. I think it's meaningful work to provide privacy law compliance so that users have the fullest autonomy
+      during their web experience that regulations allow.
     </p>
     <p class="text-soft-black dark:text-white my-4 text-lg md:text-xl leading-normal">
-      Occasionally, I do freelance frontend work on the side. I love to build UI components that people interact with on the web.
+      Aside from this work, I occasionally take on freelance frontend work. I love building clean and accessible
+      UI components that enrich everyone's web experience.
     </p>
     <p class="text-soft-black dark:text-white my-4 text-lg md:text-xl leading-normal">
-      Outside of tech, I write songs as Cuckoo and the Birds. You can listen to my music <a
+      Outside of tech, I write songs as
+      <a
+        class="rounded-sm text-purple-700 dark:text-purple-500 font-semibold hover:text-blue-500 dark-hover:text-blue-300 focus:outline-none focus:shadow-outline-light dark-focus:shadow-outline-dark"
+        >Cuckoo and the Birds</a
+      >. If you'd like, you can listen to my music
+      <a
         class="rounded-sm text-purple-700 dark:text-purple-500 font-semibold hover:text-blue-500 dark-hover:text-blue-300 focus:outline-none focus:shadow-outline-light dark-focus:shadow-outline-dark"
         href="https://cuckooandthebirds.bandcamp.com"
         rel="noreferrer"
         target="_blank"
-      >here.</a>
+        >here.</a
+      >
     </p>
-    <br>
+    <p class="text-soft-black dark:text-white my-4 text-lg md:text-xl leading-normal">
+      I'm also a very avid Star Trek fan. During these difficult times, I find comfort in its optimistic view on the potential of humanity and its future.
+    </p>
   </section>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style>
-</style>
+<style></style>
