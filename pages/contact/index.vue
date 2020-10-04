6c6e20c8293384fb9da03a8333bfde2a17012b00
Tyler Earls <tyler.a.earls@gmail.com>
2020-10-04 14:54:43 -0500

    add comments for future reference

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -128,6 +128,8 @@
 </template>
 
 <script>
+// NOTE: not converting this to TS until I can fully migrate to vue 3. 
+// the type inferences are all fucked.
 import { mapState } from "vuex";
 import VueRecaptcha from "vue-recaptcha";
 
@@ -155,7 +157,6 @@ export default {
         text: "",
       },
       message: {
-        // placeholder: "Hey Tyler,\n\nMy name is _______.\nLet's do something awesome together.",
         placeholder: "Stardate 2713.5\n\nIn the distant reaches of our galaxy, we have made an astonishing discovery – Earth-type radio signals coming from a planet which apparently is an exact duplicate of the Earth.\n\nIt seems impossible, but there it is.",
         text: "",
         error: false,
@@ -190,7 +191,7 @@ export default {
 
       // make sure the last child is a div or an iframe before removing
       // so scripts don't get removed inadvertently
-      if (/div|iframe/i.test(lastChild.nodeName)) {
+      if (lastChild != null && /div|iframe/i.test(lastChild.nodeName)) {
         lastChild.remove();
       }
     },
@@ -256,11 +257,6 @@ export default {
 .form-boxshadow {
   box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);
 }
-html.dark-mode .form-boxshadow {
-  /* box-shadow: 0 4px 6px 0 hsla(0, 100%, 100%, 0.4); */
-  /* box-shadow: 0 4px 6px 0 rgba(255, 255, 255, 0.4); */
-}
-
 
 /* transitions */
 .draw-enter-active {
