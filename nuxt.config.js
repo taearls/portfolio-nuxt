394d3f5636453f5ac8e0924dd5a4846c53b9ec77
Tyler Earls <tyler.a.earls@gmail.com>
2020-12-03 03:01:40 -0600

    fix nuxt prod build w firestore termination hook

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,26 +30,24 @@ export default {
     ],
     "@nuxt/typescript-build",
     "@nuxtjs/color-mode",
-    [
-      "@nuxtjs/firebase",
-      {
-        config: {
-          apiKey: process.env["FIREBASE_API_KEY"],
-          appId: process.env["FIREBASE_APP_ID"],
-          authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
-          databaseURL: process.env["FIREBASE_DB_URL"],
-          measurementId: process.env["FIREBASE_MEASUREMENT_ID"],
-          messagingSenderId: process.env["FIREBASE_MESSAGING_SENDER_ID"],
-          projectId: process.env["FIREBASE_PROJECT_ID"],
-          storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
-        },
-        services: {
-          firestore: true,
-        },
-      },
-    ],
+    "@nuxtjs/firebase",
     "@nuxtjs/tailwindcss",
   ],
+  firebase: {
+    config: {
+      apiKey: process.env["FIREBASE_API_KEY"],
+      appId: process.env["FIREBASE_APP_ID"],
+      authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
+      databaseURL: process.env["FIREBASE_DB_URL"],
+      measurementId: process.env["FIREBASE_MEASUREMENT_ID"],
+      messagingSenderId: process.env["FIREBASE_MESSAGING_SENDER_ID"],
+      projectId: process.env["FIREBASE_PROJECT_ID"],
+      storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+    },
+    services: {
+      firestore: true,
+    },
+  },
   babel: {
     presets() {
       return [
@@ -75,6 +73,26 @@ export default {
     { src: "~/plugins/vuelidate.js" },
     { src: "~/plugins/vue-composition-api.js" },
   ],
+  // fixes this issue where firestore doesn't terminate 
+  // https://github.com/nuxt-community/firebase-module/issues/93
+  hooks: {
+    generate: {
+      async done(builder) {
+        const appModule = await import("./.nuxt/firebase/app.js");
+        const { session } = await appModule.default(
+          builder.options.firebase.config,
+          {
+            res: null,
+          }
+        );
+        try {
+          session.firestore().terminate();
+        } catch (e) {
+          console.log(e);
+        }
+      },
+    },
+  },
   head: {
     htmlAttrs: {
       lang: "en",
